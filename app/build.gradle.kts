e3871616545d5dddf158839d9b20f308cf5ad851
mnesia11 <13306902255@189.cn>
2026-03-09 19:55:22 +0800

    更新版本号为v1.2.2

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -13,8 +13,8 @@ android {
         applicationId = "com.ustc.vacancychecker"
         minSdk = 34
         targetSdk = 35
-        versionCode = 3
-        versionName = "1.2.0"
+        versionCode = 4
+        versionName = "1.2.2"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
